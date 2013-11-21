f6e5ea2ca28a59e881b60558909ccb2776dcfaa9
vgribok <vgribok@hotmail.com>
2013-11-20 23:33:40 -0500

    Initial project setup

---
+++ Vlad.Framework.Core/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Value.Framework.Core
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Same as Object.ToString(), but supports null string too.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="nullObjString"></param>
+        /// <returns></returns>
+        public static string ToStringEx(this object obj, string nullObjString = "")
+        {
+            return obj == null ? (nullObjString ?? string.Empty) : obj.ToString();
+        }
+    }
+}
---
+++ Vlad.Framework.UnitTests/CoreTests/StringExtTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Value.Framework.Core;
+
+namespace Value.Framework.UnitTests.CoreTests
+{
+    [TestClass]
+    public class StringExtTests
+    {
+        [TestMethod]
+        public void TestNullToString()
+        {
+            string nullStr = null;
+            string actual = nullStr.ToStringEx("xoxoxo!");
+            Assert.AreEqual("xoxoxo!", actual);
+        }
+    }
+}
