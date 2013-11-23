f8e784580246e2e5c16493054f730ad97b87d506
vgribok <vgribok@hotmail.com>
2013-11-22 21:36:46 -0500

    Measured base-line empty function expression -> compile -> run time rate. Got ~4000+ calls/sec on 2.4 GHz notebook Core i7.

--- Vlad.Framework.UnitTests/AspectacularTest/AspectacularText.cs
+++ Vlad.Framework.UnitTests/AspectacularTest/AspectacularText.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Value.Framework.Aspectacular;
+using System.Diagnostics;
 
 namespace Value.Framework.UnitTests.AspectacularTest
 {
@@ -15,6 +16,17 @@ namespace Value.Framework.UnitTests.AspectacularTest
             int id = 123;
             string actual = dal.Execute<string>(ctx => () => ctx.FakeBlMethod(id));
             Assert.AreEqual("123", actual);
+        }
+
+        [TestMethod]
+        public void CallCounter()
+        {
+            var dal = new DalContext();
+            int id = 123;
+            
+            long count = RunCounter.Spin(10000, () => dal.Execute<string>(ctx => () => ctx.FakeBlMethod(id)));
+            count.ToString();
+            //Trace.WriteLine();
         }
     }
 }
---
+++ Vlad.Framework.UnitTests/RunCounter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Value.Framework.UnitTests
+{
+    public static class RunCounter
+    {
+        /// <summary>
+        /// Counts how many time certain function was executed in a given time span.
+        /// </summary>
+        /// <param name="millisecondsToRun"></param>
+        /// <param name="funcToTest"></param>
+        /// <returns></returns>
+        public static long Spin(long millisecondsToRun, Action funcToTest)
+        {
+            long count = 0, elapsed;
+
+            DateTime start = DateTime.UtcNow;
+
+            do
+            {
+                funcToTest();
+                count++;
+                elapsed = (long)(DateTime.UtcNow - start).TotalMilliseconds;
+            } while (elapsed < millisecondsToRun);
+
+            Debug.WriteLine("Ran \"{0}\" {1:#,#} times in {2:#,#} milliseconds.", funcToTest, count, millisecondsToRun);
+
+            return count;
+        }
+    }
+}
