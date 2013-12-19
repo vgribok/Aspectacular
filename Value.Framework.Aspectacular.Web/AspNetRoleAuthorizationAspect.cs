733bce2a832015d0b931655862fca30422f36abf
vgribok <vgribok@hotmail.com>
2013-12-18 22:42:39 -0500

    Added initial implementation of DemandClaimsAttribute and AuthorizationAspect that supports Claims-based authorization.

--- Value.Framework.Aspectacular.Web/AspNetRoleAuthorizationAspect.cs
+++ Value.Framework.Aspectacular.Web/AspNetRoleAuthorizationAspect.cs
@@ -5,31 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Security;
+using Value.Framework.Aspectacular.Aspects;
 using Value.Framework.Core;
 
 namespace Value.Framework.Aspectacular.Web
 {
     /// <summary>
-    /// Decorate methods and classes with this attribute
+    /// Decorate methods and classes with this attribute.
+    /// Caution: Uses Roles.GetRolesForUser() internally, which has weird caching policy.
     /// </summary>
+    /// <remarks>
+    /// For better performance, implement your own ClaimsIdentity for ASP.NET Roles 
+    /// that caches claims for longer periods of time, and then use DemandClaimsAttribute instead
+    /// of DemandAspNetRoleAttribute.
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
-    public class DemandAspNetRoleAttribute : System.Attribute
+    public class DemandAspNetRoleAttribute : AuthorizationDemandAttribute
     {
-        public bool DemandAny { get; protected set; }
-        public string[] DemandedRoles { get; protected set; }
-
         /// <summary>
         /// Marks methods and classes as requiring current user to the member of one or more ASP.NET security roles.
         /// </summary>
         /// <param name="trueDemandAny_falseDemandAll">If true, user must me a member of *at least one* role. If false, user must be a member of *all* roles.</param>
         /// <param name="DemandedRoles">Collection of roles in order to authorize access to a class or method called using AOP proxy.</param>
         public DemandAspNetRoleAttribute(bool trueDemandAny_falseDemandAll, params string[] demandedRoles)
+            : base(trueDemandAny_falseDemandAll, demandedRoles)
         {
-            if (demandedRoles.IsNullOrEmpty())
-                throw new ArgumentNullException("roles");
-
-            this.DemandAny = trueDemandAny_falseDemandAll;
-            this.DemandedRoles = demandedRoles;
         }
 
         /// <summary>
@@ -42,55 +42,25 @@ namespace Value.Framework.Aspectacular.Web
         {
         }
 
-        internal bool IsCurrentUserAuthorized()
+        protected override IEnumerable<string> GetAuthorizedClaims()
         {
-            if (this.DemandedRoles.Length == 1)
-                return Roles.IsUserInRole(this.DemandedRoles[0]);
-
             string[] userRoles = Roles.GetRolesForUser();
+            return userRoles;
+        }
 
-            int roleMemshipCount = userRoles.Intersect(this.DemandedRoles).Count();
-
-            if (this.DemandAny)
-                return roleMemshipCount > 0;
-            else
-                // demand all
-                return roleMemshipCount == this.DemandedRoles.Length;
+        public override System.Security.Principal.IIdentity Identity
+        {
+            get { return HttpContext.Current.User == null ? null : HttpContext.Current.User.Identity; }
         }
     }
 
     /// <summary>
     /// Enforces ASP.NET role membership authorization for methods or classes decorated with the DemandAspNetRoleAttribute.
     /// </summary>
-    public class AspNetRoleAuthorizationAspect : Aspect
+    public class AspNetRoleAuthorizationAspect : AuthorizationAspect
     {
-        public override void Step_2_BeforeTryingMethodExec()
+        protected override void ThrowAuthorizationException(string errorMsg)
         {
-            this.EnsureUserAspNetRoleAuthorization();
-            this.Log(EntryType.Warning, "Authorized", true.ToString());
-        }
-
-        protected void EnsureUserAspNetRoleAuthorization()
-        {
-            DemandAspNetRoleAttribute aspNetRoleDem = this.Context.InterceptedCallMetaData.GetMethodOrClassAttribute<DemandAspNetRoleAttribute>();
-
-            this.LogInformationWithKey("Demanded roles", "{0}: {1}", 
-                                    aspNetRoleDem.DemandAny ? "ANY" : "ALL",
-                                    string.Join(", ", aspNetRoleDem.DemandedRoles)
-                                    );
-
-            if (aspNetRoleDem == null)
-                return;
-
-            if (aspNetRoleDem.IsCurrentUserAuthorized())
-                return;
-
-            this.Log(EntryType.Warning, "Authorized", false.ToString());
-
-            string errorMsg = string.Format("User \"{0}\" is not authorized to call this function.", HttpContext.Current.User.Identity.Name);
-
-            this.Log(EntryType.Warning, "Authorization Failed", errorMsg);
-
             try
             {
                 HttpContext.Current.Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
@@ -100,7 +70,7 @@ namespace Value.Framework.Aspectacular.Web
                 // No biggy if status code has already be written, we can skip this.
             }
 
-            throw new Exception(errorMsg);
+            base.ThrowAuthorizationException(errorMsg);
         }
     }
 }
--- Value.Framework.Aspectacular/Aspects/ClaimsAuthorizationAspect.cs
+++ Value.Framework.Aspectacular/Aspects/ClaimsAuthorizationAspect.cs
@@ -2,54 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
-
 using Value.Framework.Core;
 
 namespace Value.Framework.Aspectacular.Aspects
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
-    public class DemandClaimsAttribute : Attribute
+    public abstract class AuthorizationDemandAttribute : Attribute
     {
         public bool DemandAny { get; protected set; }
         public string[] DemandedClaims { get; protected set; }
 
-        protected readonly Type claimsIdentityType;
-
-        public DemandClaimsAttribute(Type claimsIdentityType, bool trueDemandAny_falseDemandAll, params string[] demandedClaims)
+        protected AuthorizationDemandAttribute(bool trueDemandAny_falseDemandAll, string[] demandedClaims)
         {
-            if (claimsIdentityType == null)
-                throw new ArgumentNullException("claimsIdentityType");
+            if (demandedClaims.IsNullOrEmpty())
+                throw new ArgumentNullException("demandedClaims collection cannot be empty.");
 
-            if (!claimsIdentityType.IsSubclassOf(typeof(ClaimsPrincipal)))
-                throw new Exception("Type {0} must inherit ClaimsPrincipal to be used for authorization.".SmartFormat(claimsIdentityType.FormatCSharp()));
-
-            this.claimsIdentityType = claimsIdentityType;
             this.DemandAny = trueDemandAny_falseDemandAll;
             this.DemandedClaims = demandedClaims;
         }
 
-        public IEnumerable<string> GetAuthorizedClaims()
+        public abstract IIdentity Identity
         {
-            if (ClaimsPrincipal.Current == null)
-                return null;
-
-            ClaimsIdentity identity = ClaimsPrincipal.Current.Identities.Where(id => id.GetType() == this.claimsIdentityType).SingleOrDefault();
-            if (identity == null)
-                return null;
-
-            IEnumerable<string> authorizedCalims = identity.Claims.Select(claim => claim.Value);
-            return authorizedCalims;
+            get;
         }
 
         public bool IsAuthorized()
         {
-            if(DemandedClaims.IsNullOrEmpty())
+            if (DemandedClaims.IsNullOrEmpty())
                 return true;
 
+            return this.IsAuthorizedInternal();
+        }
+
+        protected virtual bool IsAuthorizedInternal()
+        {
             IEnumerable<string> authorizedClaims = this.GetAuthorizedClaims();
-            if(authorizedClaims == null)
+            if (authorizedClaims == null)
                 return false;
 
             int authorizedClaimCount = authorizedClaims.Where(claim => this.DemandedClaims.Contains(claim)).Count();
@@ -60,9 +51,102 @@ namespace Value.Framework.Aspectacular.Aspects
             // Demand all.
             return authorizedClaimCount == this.DemandedClaims.Length;
         }
+
+        protected abstract IEnumerable<string> GetAuthorizedClaims();
     }
 
-    //public class ClaimsAuthorizationAspect : Aspect
-    //{
-    //}
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    public class DemandClaimsAttribute : AuthorizationDemandAttribute
+    {
+        protected readonly Type claimsIdentityType;
+
+        private readonly Lazy<ClaimsIdentity> identity;
+
+        public DemandClaimsAttribute(Type claimsIdentityType, params string[] demandedClaims)
+            : this(claimsIdentityType, true, demandedClaims)
+        {
+        }
+
+        public DemandClaimsAttribute(Type claimsIdentityType, bool trueDemandAny_falseDemandAll, params string[] demandedClaims)
+            : base(trueDemandAny_falseDemandAll, demandedClaims)
+        {
+            if (claimsIdentityType == null)
+                throw new ArgumentNullException("claimsIdentityType");
+
+            if (!claimsIdentityType.IsSubclassOf(typeof(ClaimsPrincipal)))
+                throw new Exception("Type {0} must inherit ClaimsPrincipal to be used for authorization.".SmartFormat(claimsIdentityType.FormatCSharp()));
+
+            this.claimsIdentityType = claimsIdentityType;
+
+            this.identity = new Lazy<ClaimsIdentity>(() => ClaimsPrincipal.Current == null ? null : ClaimsPrincipal.Current.Identities.Where(id => id.GetType() == this.claimsIdentityType).SingleOrDefault());
+        }
+
+        public override IIdentity Identity
+        {
+            get { return this.identity.Value; }
+        }
+
+        public ClaimsIdentity ClaimsIdentity
+        {
+            get { return this.identity.Value; }
+        }
+
+        protected override IEnumerable<string> GetAuthorizedClaims()
+        {
+            if (this.ClaimsIdentity == null)
+                return null;
+
+            IEnumerable<string> authorizedClaims = this.ClaimsIdentity.Claims.Where(claim => claim.Type == this.ClaimsIdentity.RoleClaimType).Select(claim => claim.Value);
+            return authorizedClaims;
+        }
+
+        protected override bool IsAuthorizedInternal()
+        {
+            if (!this.Identity.IsAuthenticated)
+                return false;
+
+            return base.IsAuthorizedInternal();
+        }
+    }
+
+    /// <summary>
+    /// Enforces claims/roles authorization for methods or classes decorated with subclasses of the AuthorizationDemandAttribute.
+    /// </summary>
+    public class AuthorizationAspect : Aspect
+    {
+        public override void Step_2_BeforeTryingMethodExec()
+        {
+            this.EnsureAuthorization();
+            this.Log(EntryType.Warning, "Authorized", true.ToString());
+        }
+
+        protected void EnsureAuthorization()
+        {
+            AuthorizationDemandAttribute claimDemandAttrib = this.Context.InterceptedCallMetaData.GetMethodOrClassAttribute<AuthorizationDemandAttribute>();
+
+            this.LogInformationWithKey("Demanded claims/roles", "{0}: {1}",
+                                    claimDemandAttrib.DemandAny ? "ANY" : "ALL",
+                                    string.Join(", ", claimDemandAttrib.DemandedClaims)
+                                    );
+
+            if (claimDemandAttrib == null)
+                return;
+
+            if (claimDemandAttrib.IsAuthorized())
+                return;
+
+            this.Log(EntryType.Warning, "Authorized", false.ToString());
+
+            string errorMsg = string.Format("User \"{0}\" is not authorized to call this function.", claimDemandAttrib.Identity.Name);
+
+            this.Log(EntryType.Warning, "Authorization Failed", errorMsg);
+
+            this.ThrowAuthorizationException(errorMsg);
+        }
+
+        protected virtual void ThrowAuthorizationException(string errorMsg)
+        {
+            throw new Exception(errorMsg);
+        }
+    }
 }
