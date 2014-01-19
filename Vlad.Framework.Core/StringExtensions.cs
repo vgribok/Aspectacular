bdc70fbb490c545db2d25aa5a648806e3fc07374
vgribok <vgribok@hotmail.com>
2014-01-19 18:33:50 -0500

    Added value type parsing methods.

--- Vlad.Framework.Core/StringExtensions.cs
+++ Vlad.Framework.Core/StringExtensions.cs
@@ -43,5 +43,154 @@ namespace Aspectacular
 
             return string.Format(format, args);
         }
+
+        #region Value type parsing
+
+        /// <summary>
+        /// Parses string and returns null if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static byte? ParseByte(this string str)
+        {
+            byte val;
+            if (byte.TryParse(str, out val))
+                return val;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Parses string and returns default value if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static byte ParseByte(this string str, byte defaultValue)
+        {
+            byte val;
+            if (byte.TryParse(str, out val))
+                return val;
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Parses string and returns null if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int? ParseInt(this string str)
+        {
+            int val;
+            if (int.TryParse(str, out val))
+                return val;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Parses string and returns default value if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static int Parse(this string str, int defaultValue)
+        {
+            int val;
+            if (int.TryParse(str, out val))
+                return val;
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Parses string and returns null if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool? ParseBool(this string str)
+        {
+            bool val;
+            if (bool.TryParse(str, out val))
+                return val;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Parses string and returns default value if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static bool Parse(this string str, bool defaultValue)
+        {
+            bool val;
+            if (bool.TryParse(str, out val))
+                return val;
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Parses string and returns null if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static decimal? ParseDecimal(this string str)
+        {
+            decimal val;
+            if (decimal.TryParse(str, out val))
+                return val;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Parses string and returns default value if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static decimal Parse(this string str, decimal defaultValue)
+        {
+            decimal val;
+            if (decimal.TryParse(str, out val))
+                return val;
+
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Parses string and returns null if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static double? ParseDouble(this string str)
+        {
+            double val;
+            if (double.TryParse(str, out val))
+                return val;
+
+            return null;
+        }
+
+        /// <summary>
+        /// Parses string and returns default value if parsing failed.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static double Parse(this string str, double defaultValue)
+        {
+            double val;
+            if (double.TryParse(str, out val))
+                return val;
+
+            return defaultValue;
+        }
+
+        #endregion Value type parsing
     }
 }
