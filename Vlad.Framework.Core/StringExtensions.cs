fb937f8b525cd3ccb9c0d706ab32043dafc10e97
Vlad Hrybok <vgribok@hotmail.com>
2014-11-10 16:23:40 -0500

    Nevermind.

--- Vlad.Framework.Core/StringExtensions.cs
+++ Vlad.Framework.Core/StringExtensions.cs
@@ -472,16 +472,5 @@ namespace Aspectacular
                 return obj;
             }
         }
-
-        /// <summary>
-        /// Extension wrapper for Enum.Parse(Type,string).
-        /// </summary>
-        /// <typeparam name="T">Enum type</typeparam>
-        /// <param name="str">string to parse</param>
-        /// <returns></returns>
-        public static T ParseEnum<T>(this string str) where T : struct
-        {
-            return (T)Enum.Parse(typeof(T), str);
-        }
     }
 }
