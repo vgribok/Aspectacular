07c0ffae91a9489dfa4c844d036975eec7bf62eb
Vlad Hrybok <vgribok@hotmail.com>
2015-06-02 11:39:46 -0400

    Created NuGet projects for 4.0 - for no good reason, need re-do to make it universal

---
+++ NET 4.0/NuGet Projects/NuGet UnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿#region License Info Header
+
+// This file is a part of the Aspectacular framework created by Vlad Hrybok.
+// This software is free and is distributed under MIT License: http://bit.ly/Q3mUG7
+
+#endregion
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NuGet_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string str = "Hello, World!";
+
+            //int index = str.GetProxy().Invoke(s => s.IndexOf("lo")); // Don't forget to uncomment default aspect parts in the app.config.
+            //Assert.AreEqual(3, index);
+        }
+    }
+}
