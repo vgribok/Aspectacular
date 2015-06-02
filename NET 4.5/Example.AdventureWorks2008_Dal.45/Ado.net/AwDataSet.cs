30cf275198f074bda65446d6b5fb99598878a2cf
Vlad Hrybok <vgribok@hotmail.com>
2015-06-02 14:30:26 -0400

    Combined NuGet projects into a single one, comprising both 4.0 and 4.5 binaries.

---
+++ NET 4.5/Example.AdventureWorks2008_Dal.45/Ado.net/AwDataSet.cs
@@ -0,0 +1,28 @@
+﻿#region License Info Header
+
+// This file is a part of the Aspectacular framework created by Vlad Hrybok.
+// This software is free and is distributed under MIT License: http://bit.ly/Q3mUG7
+
+#endregion
+
+using System.Data.SqlClient;
+using Aspectacular;
+
+namespace Example.AdventureWorks2008ObjectContext_Dal.Ado.net
+{
+    public partial class AwDataSet
+    {
+    }
+}
+
+namespace Example.AdventureWorks2008ObjectContext_Dal.Ado.net.AwDataSetTableAdapters
+{
+    [InvariantReturn]
+    public partial class ProductCategoryTableAdapter : ISqlServerConnectionProvider
+    {
+        public SqlConnection SqlConnection
+        {
+            get { return this.Connection; }
+        }
+    }
+}
---
+++ NuGet Projects/!Readme.txt
@@ -0,0 +1,5 @@
+First, build in Release mode following solutions:
+1) ..\NET 4.0\AspectacularFramework.40.sln
+2) ..\NET 4.5\AspectacularFramework.45.sln
+
+Then build this solution in Release mode.
